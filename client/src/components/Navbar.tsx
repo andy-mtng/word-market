19224c330ce7d75324f6ef1c8a44af5b9c5a366c
Andy N <andynguyen9001@gmail.com>
2023-06-28 20:00:42 -0600

    Adjust navbar styles

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,22 +1,33 @@
 import { Link } from "react-router-dom";
+import DefaultPfp from "../assets/default_pfp.png";
 
 function Navbar(): JSX.Element {
     return (
         <nav className="flex items-center justify-between px-12 py-6">
             <Link to="/">
-                <div className="flex items-center gap-3">
-                    <div className="font-merriweather flex h-9 w-9 items-center justify-center rounded-md bg-green-700 text-2xl font-bold text-white">
+                <div className="flex items-center gap-2">
+                    <div className="font-merriweather flex h-9 w-9 items-center justify-center rounded-md bg-green-900 pt-1 text-2xl font-bold text-white">
                         W
                     </div>
                     <h1 className="text-2xl font-bold">WordMarket</h1>
                 </div>
             </Link>
-            <ul className="flex gap-4 text-lg font-medium">
+            <ul className="flex items-center gap-4 text-lg font-medium">
                 <li>
-                    <Link to="/profile">Profile</Link>
+                    <Link to="/explore">Explore</Link>
                 </li>
                 <li>
-                    <Link to="/login">Login</Link>
+                    <Link to="/profile">
+                        <div className="box-border h-10 w-10 rounded-md bg-gray-300"></div>
+                    </Link>
+                </li>
+                <li>
+                    <Link
+                        className="rounded-sm bg-green-700 px-6 py-2 text-white shadow-sm"
+                        to="/login"
+                    >
+                        Login
+                    </Link>
                 </li>
             </ul>
         </nav>
