5dc97adac169977d4efc6db9c1a32617533dbf5a
Andy N <andynguyen9001@gmail.com>
2023-06-29 21:23:57 -0600

    Connect client side login to server

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,7 +1,65 @@
+import { useForm, SubmitHandler } from "react-hook-form";
+import axios from "axios";
+import { Link } from "react-router-dom";
+
+interface Inputs {
+    email: string;
+    password: string;
+}
+
 function Login(): JSX.Element {
+    const {
+        register,
+        handleSubmit,
+        reset,
+        formState: { errors }
+    } = useForm<Inputs>();
+
+    const onSubmit: SubmitHandler<Inputs> = (formData) => {
+        console.log("Client-side formData", formData);
+        axios
+            .post("http://localhost:5000/auth/login", formData)
+            .then((response) => {
+                console.log(response);
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+            .finally(() => {
+                reset();
+            });
+    };
+
     return (
         <div>
-            <h1>Login Page</h1>
+            <h1>Login</h1>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <div>
+                    <label htmlFor="email">Email</label>
+                    <input
+                        className="border border-gray-400 "
+                        id="email"
+                        {...register("email", { required: true })}
+                    />
+                </div>
+                {errors.email && <span>Email is required</span>}
+
+                <div>
+                    <label htmlFor="password">Password</label>
+                    <input
+                        type="password"
+                        className="border border-gray-400"
+                        id="password"
+                        {...register("password", { required: true })}
+                    />
+                </div>
+                {errors.password && <span>Password is required</span>}
+
+                <button type="submit">Login</button>
+            </form>
+            <p>
+                New to WordMarket? <Link to="/signup">Join now</Link>
+            </p>
         </div>
     );
 }
